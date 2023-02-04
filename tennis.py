2d012dc4fe846cc2ef001379b4564bb317dfa103
Andrew Nzioki <acnzioki5@gmail.com>
2023-02-04 12:31:58 +0300

    Made a new dataframe with ordered data

--- tennis.py
+++ tennis.py
@@ -50,8 +50,47 @@ the players with their rankings
 #print(tennis['Player'].nunique())
 #438 unique tennis players
 
-grouped_tennis = tennis.groupby('Player')
-print(grouped_tennis)
+tennis.sort_values(by='Player', ascending=True, inplace=True)
+#print(tennis.head())
+
+#print(tennis.Player.unique())
+
+# group the dataframe by 'Player' and 'Ranking' columns
+grouped = tennis.groupby(by=['Player', 'Ranking'])
+
+# calculate the mean score for each group
+tennis_averages = grouped[['FirstServe','FirstServePointsWon',
+'FirstServeReturnPointsWon', 'SecondServePointsWon','SecondServeReturnPointsWon'
+, 'BreakPointsConverted','BreakPointsSaved','ReturnGamesWon','ReturnPointsWon'
+,'TotalPointsWon','TotalServicePointsWon']].mean()
+
+# display the mean scores for each group
+#print(tennis_averages)
+
+#Do the same for the sums
+tennis_sums = grouped[['Aces','BreakPointsFaced','BreakPointsOpportunities',
+'DoubleFaults','ReturnGamesPlayed','ServiceGamesPlayed','Wins','Losses',
+'Winnings']].sum()
+#print(tennis_sums)
+
+'''
+We also know that this data is for players over a number of years, so we also 
+count the number of years played
+'''
+tennis_count = grouped['Year'].count()
+print(tennis_count)
+
+#Now we merge all the data
+tennis_updated = pd.merge(tennis_averages, tennis_sums, 
+                          on=['Player', 'Ranking'], how='inner')
+tennis_updated2 = pd.merge(tennis_updated, tennis_count, 
+                          on=['Player', 'Ranking'], how='inner')
+
+
+
+
+
+
 
 
 
